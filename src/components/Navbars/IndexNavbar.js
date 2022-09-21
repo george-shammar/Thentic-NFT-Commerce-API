9090a6e1018160760c09fe80895ac8e100e57a41
george-shammar <george.gbenle@outlook.com>
2022-09-21 21:48:53 +0100

    Connect wallet

--- src/components/Navbars/IndexNavbar.js
+++ src/components/Navbars/IndexNavbar.js
@@ -54,7 +54,7 @@ function IndexNavbar() {
             target="_blank"
             title="Coded by Creative Tim"
           >
-            Paper Kit React
+            Demo Thentic
           </NavbarBrand>
           <button
             aria-expanded={navbarCollapse}
@@ -77,7 +77,7 @@ function IndexNavbar() {
             <NavItem>
               <NavLink
                 data-placement="bottom"
-                href="https://twitter.com/CreativeTim?ref=creativetim"
+                href=""
                 target="_blank"
                 title="Follow us on Twitter"
               >
@@ -88,7 +88,7 @@ function IndexNavbar() {
             <NavItem>
               <NavLink
                 data-placement="bottom"
-                href="https://www.facebook.com/CreativeTim?ref=creativetim"
+                href=""
                 target="_blank"
                 title="Like us on Facebook"
               >
@@ -99,7 +99,7 @@ function IndexNavbar() {
             <NavItem>
               <NavLink
                 data-placement="bottom"
-                href="https://www.instagram.com/CreativeTimOfficial?ref=creativetim"
+                href=""
                 target="_blank"
                 title="Follow us on Instagram"
               >
@@ -110,7 +110,7 @@ function IndexNavbar() {
             <NavItem>
               <NavLink
                 data-placement="bottom"
-                href="https://www.github.com/CreativeTimOfficial/paper-kit-react?ref=creativetim"
+                href=""
                 target="_blank"
                 title="Star on GitHub"
               >
@@ -120,7 +120,7 @@ function IndexNavbar() {
             </NavItem>
             <NavItem>
               <NavLink
-                href="https://demos.creative-tim.com/paper-kit-react/#/documentation?ref=pkr-index-navbar"
+                href=""
                 target="_blank"
               >
                 <i className="nc-icon nc-book-bookmark" /> Documentation
@@ -130,10 +130,10 @@ function IndexNavbar() {
               <Button
                 className="btn-round"
                 color="danger"
-                href="https://www.creative-tim.com/product/paper-kit-pro-react?ref=pkr-index-navbar"
+                href=""
                 target="_blank"
               >
-                <i className="nc-icon nc-spaceship"></i> Upgrade to Pro
+                <i className="nc-icon nc-spaceship"></i> Connect Wallet
               </Button>
             </NavItem>
           </Nav>
