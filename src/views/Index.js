310e97fe18b424a16947b6518b7263375304efaa
george-shammar <george.gbenle@outlook.com>
2022-09-21 22:12:42 +0100

    Delete unwantd files

--- src/views/Index.js
+++ src/views/Index.js
@@ -25,21 +25,6 @@ import IndexNavbar from "components/Navbars/IndexNavbar.js";
 import IndexHeader from "components/Headers/IndexHeader.js";
 import DemoFooter from "components/Footers/DemoFooter.js";
 
-// index sections
-import SectionButtons from "views/index-sections/SectionButtons.js";
-import SectionNavbars from "views/index-sections/SectionNavbars.js";
-import SectionNavigation from "views/index-sections/SectionNavigation.js";
-import SectionProgress from "views/index-sections/SectionProgress.js";
-import SectionNotifications from "views/index-sections/SectionNotifications.js";
-import SectionTypography from "views/index-sections/SectionTypography.js";
-import SectionJavaScript from "views/index-sections/SectionJavaScript.js";
-import SectionCarousel from "views/index-sections/SectionCarousel.js";
-import SectionNucleoIcons from "views/index-sections/SectionNucleoIcons.js";
-import SectionDark from "views/index-sections/SectionDark.js";
-import SectionLogin from "views/index-sections/SectionLogin.js";
-import SectionExamples from "views/index-sections/SectionExamples.js";
-import SectionDownload from "views/index-sections/SectionDownload.js";
-import Thentic from "views/index-sections/Thentic.js";
 
 function Index() {
   document.documentElement.classList.remove("nav-open");
@@ -54,20 +39,6 @@ function Index() {
       <IndexNavbar />
       <IndexHeader />
       <div className="main">
-        <Thentic />
-        {/* <SectionButtons />
-        <SectionNavbars />
-        <SectionNavigation /> */}
-        {/* <SectionProgress />
-        <SectionNotifications />
-        <SectionTypography />
-        <SectionJavaScript />
-        <SectionCarousel />
-        <SectionNucleoIcons />
-        <SectionDark />
-        <SectionLogin />
-        <SectionExamples />
-        <SectionDownload /> */}
         <DemoFooter />
       </div>
     </>
