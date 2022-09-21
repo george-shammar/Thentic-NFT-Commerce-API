b360b12fdeee00a508a847fe274660fe4127f98a
george-shammar <george.gbenle@outlook.com>
2022-09-21 22:01:22 +0100

    Add mint button

--- src/views/index-sections/Thentic.js
+++ src/views/index-sections/Thentic.js
@@ -1,9 +1,18 @@
 import React from "react";
 
-function SectionButtons() {
+function Thentic() {
   return(
     <div>
-      
+            <Button
+                className="btn-round"
+                color="danger"
+                href=""
+                target="_blank"
+              >
+                <i className="nc-icon nc-spaceship"></i> Mint
+              </Button>
     </div>
   )
 }
+
+export default Thentic;
